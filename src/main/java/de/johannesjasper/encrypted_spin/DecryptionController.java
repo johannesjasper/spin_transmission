04dc298770cbd476fb5568d566ebb6c077a7e05d
Johannes Jasper <johannes.jasper.ext@syrocon.de>
2021-11-04 14:22:31 +0100

    Error handling

--- src/main/java/de/johannesjasper/encrypted_spin/DecryptionController.java
+++ src/main/java/de/johannesjasper/encrypted_spin/DecryptionController.java
@@ -3,25 +3,51 @@ package de.johannesjasper.encrypted_spin;
 import com.nimbusds.jose.JWEAlgorithm;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import java.security.Key;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.Base64;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequiredArgsConstructor
+@Slf4j
 public class DecryptionController {
 
     public record DecryptionRequest(String ciphertext) {
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public class InvalidSpinException extends RuntimeException {
+        public InvalidSpinException(String message) {
+            super(message);
+        }
+    }
+
     private final SPINConfig config;
+    private final Cipher cipher;
+
+    @SneakyThrows
+    public DecryptionController(SPINConfig config) {
+        this.config = config;
+        try {
+            this.cipher = Cipher.getInstance("RSA/None/OAEPWithSHA512AndMGF1Padding", "BC");
+            cipher.init(Cipher.DECRYPT_MODE, config.getPrivateKey());
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException e) {
+            log.error("Unable to initialize Cipher.", e);
+            throw e;
+        }
+    }
+
 
     @GetMapping("/jwk")
     public Map<String, Object> getJwk() {
@@ -36,13 +62,16 @@ public class DecryptionController {
 
 
     @PostMapping("/decrypt")
-    public String decrypt(@RequestBody DecryptionRequest request) throws Exception {
-        return decrypt(request.ciphertext, config.getPrivateKey());
+    public String decrypt(@RequestBody DecryptionRequest request) {
+        return decrypt(request.ciphertext).orElseThrow(() -> new InvalidSpinException("Could not decrypt ciphertext"));
     }
 
-    private String decrypt(String cipherText, Key privKey) throws Exception {
-        var cipher = Cipher.getInstance("RSA/None/OAEPWithSHA512AndMGF1Padding", "BC");
-        cipher.init(Cipher.DECRYPT_MODE, privKey);
-        return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)));
+    private Optional<String> decrypt(String cipherText) {
+        try {
+            return Optional.of(new String(cipher.doFinal(Base64.getDecoder().decode(cipherText))));
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            log.error("Could not decrypt ciphertext", e);
+            return Optional.empty();
+        }
     }
 }
